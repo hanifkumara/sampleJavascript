22e7f8d98ac1d0a53763acc466098557bc8026c5
hanif <hnflasting@gmail.com>
2020-10-27 12:13:08 +0700

    soal-2

---
+++ tugas-introjs2/soal-2/script.js
@@ -0,0 +1,17 @@
+const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
+    if (nilaiAwal > nilaiAkhir) {
+        console.log("Nilai akhir harus lebih besar dari nilai awal")
+    } else if(dataArray.length < 5) {
+        console.log("Jumlah angka dalam dataArray tidak ada")
+    } else {
+        let coba = dataArray.filter(value => {
+            if (value > nilaiAwal && value < nilaiAkhir) {
+                return value
+            }
+        })
+        coba.sort((a, b) => a - b)
+        console.log(coba)
+    }
+}
+
+seleksiNilai(5,20,[2,25,4,14,17,30,8])
